d31fe6164dbb801ec2f241784818e2a6ff5b0bb8
mes32 <stockman.mike@gmail.com>
2018-01-07 09:03:58 -0800

    feature: Print the game title.

---
+++ src/main/java/cryptophasia/LaunchGame.java
@@ -0,0 +1,23 @@
+/*
+    LaunchGame.java
+
+    Launcher class for the game "Engines of Creation: Cryptophasia"
+ */
+
+package cryptophasia;
+
+import java.io.*;
+
+public class LaunchGame {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("+-----------------------------------+");
+        System.out.println("| Engines of Creation: Cryptophasia |");
+        System.out.println("+-----------------------------------+");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
